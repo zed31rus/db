b611719a674710ae4dfd4547dd35275f7f939d15
zed31rus-LAPTOP <olkozack@gmail.com>
2026-04-02 22:49:57 +0300

    .

--- web/features/account/account.dto.ts
+++ web/features/account/account.dto.ts
@@ -17,6 +17,13 @@ const AccountSchemas = {
             submitCode: z.string().length(6),
             password: z.string().min(8)
         })
+    },
+    changeAvatar: {
+        Form: z.object({
+            avatar: z.instanceof(File)
+                .refine((file) => file.size <= 20 * 1024 * 1024, 'Max size is 20MB')
+                .refine((file) => file.type.startsWith('image/'), 'Only images are allowed')
+        })
     }
 }
 
