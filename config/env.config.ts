421f10596e8ca22cb8da7f98c8ff9447f43652de
zed31rus_ <olkozack@gmail.com>
2026-04-28 22:04:10 +0300

    env

--- config/env.config.ts
+++ config/env.config.ts
@@ -6,35 +6,49 @@ import PrismaError from "#root/errors/prisma.errors.js";
 dotenv.config();
 
 export default class EnvConfig {
-    private getEnv(key: string) {
+    readonly DATABASE_URL: string;
+    readonly SMTP_API_KEY: string;
+    readonly PORT: string;
+    readonly SMTP_HOST: string;
+    readonly SMTP_USER: string;
+    readonly JWT_SECRET: string;
+    readonly SMTP_EMAIL: string;
+    readonly AVATARS_PUBLIC_DIR_PATH: string;
+    readonly PUBLIC_DIR_PATH: string;
+    readonly AMQP_URL: string;
+    readonly DISCORD_OAUTH_CLIENT_ID: string;
+    readonly DISCORD_OAUTH_CLIENT_SECRET: string;
+    readonly DISCORD_OAUTH_REDIRECT_URL: string;
+    readonly SMTP_PORT: string;
+
+    constructor(
+        readonly errors: {
+            api: ApiError,
+            config: ConfigError,
+            prisma: PrismaError
+        }
+    ) {
+        this.DATABASE_URL = this.getEnv('DATABASE_URL');
+        this.SMTP_API_KEY = this.getEnv('SMTP_API_KEY');
+        this.PORT = this.getEnv('PORT');
+        this.SMTP_HOST = this.getEnv('SMTP_HOST');
+        this.SMTP_USER = this.getEnv('SMTP_USER');
+        this.JWT_SECRET = this.getEnv('JWT_SECRET');
+        this.SMTP_EMAIL = this.getEnv('SMTP_EMAIL');
+        this.AVATARS_PUBLIC_DIR_PATH = this.getEnv('AVATARS_PUBLIC_DIR_PATH');
+        this.PUBLIC_DIR_PATH = this.getEnv('PUBLIC_DIR_PATH');
+        this.AMQP_URL = this.getEnv('AMQP_URL');
+        this.DISCORD_OAUTH_CLIENT_ID = this.getEnv('DISCORD_CLIENT_ID');
+        this.DISCORD_OAUTH_CLIENT_SECRET = this.getEnv('DISCORD_CLIENT_SECRET');
+        this.DISCORD_OAUTH_REDIRECT_URL = this.getEnv('DISCORD_REDIRECT_URL');
+        this.SMTP_PORT = this.getEnv('SMTP_PORT');
+    }
+
+    private getEnv(key: string): string {
         const value = process.env[key];
         if (!value) {
             throw this.errors.config.env(`Missing environment variable: ${key}`);
         }
         return value;
     }
-
-    DATABASE_URL = this.getEnv('DATABASE_URL');
-    SMTP_API_KEY = this.getEnv('SMTP_API_KEY');
-    PORT = this.getEnv('PORT');
-    SMTP_HOST = this.getEnv('SMTP_HOST');
-    SMTP_USER = this.getEnv('SMTP_USER');
-    JWT_SECRET = this.getEnv('JWT_SECRET');
-    SMTP_EMAIL = this.getEnv('SMTP_EMAIL');
-    
-    AVATARS_PUBLIC_DIR_PATH = this.getEnv('AVATARS_PUBLIC_DIR_PATH');
-    PUBLIC_DIR_PATH = this.getEnv('PUBLIC_DIR_PATH');
-    AMQP_URL = this.getEnv('AMQP_URL')
-    DISCORD_OAUTH_CLIENT_ID = this.getEnv('DISCORD_CLIENT_ID');
-    DISCORD_OAUTH_CLIENT_SECRET = this.getEnv('DISCORD_CLIENT_SECRET');
-    DISCORD_OAUTH_REDIRECT_URL = this.getEnv('DISCORD_REDIRECT_URL');
-    SMTP_PORT = this.getEnv('SMTP_PORT')
-
-    constructor(        
-        readonly errors: {
-            api: ApiError,
-            config: ConfigError,
-            prisma: PrismaError
-        }
-    ) {}
 }
